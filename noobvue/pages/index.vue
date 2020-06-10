95cc92dfe1ac8eb710b8c35b0c1c86f680fd5f1e
Nikhil R <nikhil.raut94@gmail.com>
2020-06-10 21:08:29 +0530

    init

---
+++ noobvue/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div class="container">
+    <HeaderMenu/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HeaderMenu from '~/components/HeaderMenu.vue'
+
+export default Vue.extend({
+  components: {
+    HeaderMenu,
+  }
+})
+</script>
+
+<style>
+
+</style>
