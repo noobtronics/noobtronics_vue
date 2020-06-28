be1b8d35bd870add7f989abdbfcf23fcad59ac30
Nikhil R <nikhil.raut94@gmail.com>
2020-06-28 15:47:50 +0530

    Update lazysizes.js

--- noobnuxt/plugins/lazysizes.js
+++ noobnuxt/plugins/lazysizes.js
@@ -1,4 +1,6 @@
 import lazySizes from 'lazysizes'
 import 'lazysizes/plugins/attrchange/ls.attrchange'
+import 'lazysizes/plugins/blur-up/ls.blur-up'
 
 lazySizes.cfg.lazyClass = 'lazyload'
+lazySizes.cfg.blurupMode = 'auto'
