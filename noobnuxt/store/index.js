3af38c85ebe31738f87726e38aa1fe930909258d
Nikhil R <nikhil.raut94@gmail.com>
2020-06-28 21:52:08 +0530

    removed store

--- noobnuxt/store/index.js
+++ noobnuxt/store/index.js
@@ -1,26 +1,5 @@
 export const state = () => ({
-  navbar: {
-    MenuBar: false,
-    SearchBar: false,
-    CartBar: false,
-    AccountBar: false
-  },
   cart: {
     cart_count: 0
   }
 })
-
-export const mutations = {
-  navbar_toggleMenuBar(state) {
-    state.navbar.MenuBar = !state.navbar.MenuBar
-  },
-  navbar_toggleSearchBar(state) {
-    state.navbar.SearchBar = !state.navbar.SearchBar
-  },
-  navbar_toggleCartBar(state) {
-    state.navbar.CartBar = !state.navbar.CartBar
-  },
-  navbar_toggleAccountBar(state) {
-    state.navbar.AccountBar = !state.navbar.AccountBar
-  }
-}
