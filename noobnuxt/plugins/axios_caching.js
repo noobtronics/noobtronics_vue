6554daafad2a4c7999099b43906590d924907420
Nikhil R <nikhil.raut94@gmail.com>
2020-06-28 15:49:37 +0530

    s

---
+++ noobnuxt/plugins/axios_caching.js
@@ -0,0 +1,9 @@
+import { cacheAdapterEnhancer } from 'axios-extensions'
+
+export default function({ $axios, ssrContext }) {
+  const defaults = $axios.defaults
+  defaults.adapter = cacheAdapterEnhancer(defaults.adapter, {
+    enabledByDefault: false,
+    cacheFlag: 'useCache'
+  })
+}
