8f68557c13952e14d4d54df27637994e8df5d185
Nikhil R <nikhil.raut94@gmail.com>
2020-07-15 22:14:45 +0530

    Update error.vue

--- noobnuxt/layouts/error.vue
+++ noobnuxt/layouts/error.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container fullwdithcontainer">
-    <HeaderMenu />
     <div
       class="content has-text-centered"
       style="height: 400px; padding: 100px 0px;"
@@ -9,19 +8,12 @@
       <h2>Please refresh this page or go to another url.</h2>
       <nuxt-link to="/">Home page</nuxt-link>
     </div>
-    <Footer />
   </div>
 </template>
 
 <script>
-import HeaderMenu from '~/components/HeaderMenu.vue'
-import Footer from '~/components/Footer.vue'
-
 export default {
-  components: {
-    HeaderMenu,
-    Footer,
-  },
+  components: {},
   layout: 'blog', // you can set a custom layout for the error page
 }
 </script>
