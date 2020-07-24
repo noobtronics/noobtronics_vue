b93f0a8a7433a6510ac9dfbb3d80731ad79649e1
Nikhil R <nikhil.raut94@gmail.com>
2020-07-24 22:18:02 +0530

    Update notify.js

--- noobnuxt/plugins/notify.js
+++ noobnuxt/plugins/notify.js
@@ -18,6 +18,15 @@ export default (context, inject) => {
         timeout: 2000,
       })
     }
+    if (status === 'cartadded') {
+      this.$store.dispatch('insertNotification', {
+        data: {
+          class: 'is-info is-light',
+          message: '<b>Added to Cart!</b><br>' + message,
+        },
+        timeout: 2000,
+      })
+    }
   }
   // Inject $hello(msg) in Vue, context and store.
   inject('notify', notify)
